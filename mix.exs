882ce3b5d54feea6a2b48da0f3ff759241411c2a
Artem Baikov <ivankolga25@gmail.com>
2023-12-26 16:37:30 +0300

    install deps

--- mix.exs
+++ mix.exs
@@ -21,6 +21,10 @@ defmodule QuantumStorageRedis.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:quantum, ">= 2.2.1"},
+      {:timex, "~> 3.0"},
+      {:redix, "~> 1.1"},
+      {:castore, ">= 0.0.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
