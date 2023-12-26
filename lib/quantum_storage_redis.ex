ba5a89493a3a2b70285e0ce898b6db93cb5b2a6c
Artem Baikov <ivankolga25@gmail.com>
2023-12-26 16:35:58 +0300

    Initial commit

---
+++ lib/quantum_storage_redis.ex
@@ -0,0 +1,18 @@
+defmodule QuantumStorageRedis do
+  @moduledoc """
+  Documentation for `QuantumStorageRedis`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> QuantumStorageRedis.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/quantum_storage_redis_test.exs
@@ -0,0 +1,8 @@
+defmodule QuantumStorageRedisTest do
+  use ExUnit.Case
+  doctest QuantumStorageRedis
+
+  test "greets the world" do
+    assert QuantumStorageRedis.hello() == :world
+  end
+end
